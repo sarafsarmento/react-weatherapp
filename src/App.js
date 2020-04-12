ba417ba5fcc67df5f70ff5116129f28781a14a5b
sarafsarmento <sarafsarmento@gmail.com>
2020-04-12 20:29:30 +0100

    Added Github repo URL

--- src/App.js
+++ src/App.js
@@ -3,7 +3,20 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        <a
+          href="https://github.com/sarafsarmento/react-weatherapp"
+          target="_blank"
+        >
+          Open-source code
+        </a>{" "}
+        by Sara Sarmento
+      </footer>
+    </div>
+  );
 }
 
 export default App;
